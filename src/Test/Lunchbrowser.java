908ad48513b015bfd9f7f0654bf0d0475b65a5ec
PratimaAutomation <pratimawaghmare26.com>
2019-11-16 12:59:57 +0530

    First seleniumproject

---
+++ src/Test/Lunchbrowser.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Lunchbrowser {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/Test/findElementsTags.java
@@ -0,0 +1,36 @@
+package Test;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class findElementsTags {
+
+	public static void main(String[] args) throws InterruptedException {
+				
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Swapnil\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.amazon.com");
+		
+		List <WebElement> linksList= driver.findElements(By.tagName("a"));
+		
+		System.out.println(linksList.size());
+		for(int i=0; i<linksList.size(); i++){
+			System.out.println(linksList.get(i).getText());
+			
+			/*String linkText=linksList.get(i).getText();
+			//Thread.sleep(5000);
+		if(linkText.equals("Your Amazon.com"));
+			{
+				linksList.get(i).click();
+				break;
+			}*/
+		}
+		//System.out.println("total links are: " + linksList.size());*/
+
+	}
+
+}

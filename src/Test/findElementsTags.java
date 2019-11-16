37634e327eca06c7655755f55aa9d925d321a57f
PratimaAutomation <57825007+PratimaAutomation@users.noreply.github.com>
2019-11-16 13:11:44 +0530

    added comment

--- src/Test/findElementsTags.java
+++ src/Test/findElementsTags.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class findElementsTags {
-
+//element tags
 	public static void main(String[] args) throws InterruptedException {
 				
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Swapnil\\Downloads\\chromedriver_win32\\chromedriver.exe");
